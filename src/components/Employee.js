c5b4e495ef1dc8752a8cecf5df1fae8f0d0a475b
LZ <laraezegart@gmail.com>
2021-03-15 15:43:57 -0700

    added sort and filter functions

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,15 +1,17 @@
 import React from "react";
 import EmployeeDirectory from "../pages/EmployeeDirectory";
 import "./Employee.css";
+import moment from "moment";
 
 function Employee(props) {
+    const dateOfBirth = moment(props.user.dob.date).format('MMMM Do YYYY')
     return (
         <div className="empRow">
             <img className="image" src={props.user.picture.thumbnail}/>
             <p className="name">{props.user.name.first} {props.user.name.last}</p>
             <p className="email">{props.user.email}</p>
             <p className="phone">{props.user.phone}</p>
-            <p className="birthday">{props.user.dob.date}</p>
+            <p className="birthday">{dateOfBirth}</p>
         </div>
     )
 }
--- src/pages/EmployeeDirectory.js
+++ src/pages/EmployeeDirectory.js
@@ -2,50 +2,86 @@ import React, {useEffect, useState} from "react";
 import API from "../utils/API";
 import Employee from "../components/Employee";
 import SearchEmp from "../components/SearchEmployee";
+import moment from "moment";
 
 
 
 function EmployeeDirectory() {
     const [state, setState] = useState({ users: [], filterUsers: [] })
     useEffect( () => {
-        console.log("hello")
+        // console.log("hello")
         API.getRandomUsers()
             .then(res => {
-                console.log(res.data.results);
+                // console.log(res.data.results);
                 setState(
                     {users: res.data.results, filterUsers: res.data.results}
                 )
-                console.log(state);
+                // console.log(state);
             })
 
     }, [])
-    function handleSearch() {
-        const searchedUsers = state.users.filter(user => {
-            if (user.name.first.includes("a"))
-                return true
-            else 
-                return false     
+    function sortByDOB() {
+        const sortedUsers = state.users.sort((current, next) => {
+            const currentDOB = moment(current.dob.date);
+            const nextDOB = moment(next.dob.date);
+            return nextDOB.diff(currentDOB)
         })
+    setState(
+        {
+            ...state,
+            filterUsers: sortedUsers
+        }
+    )
+    }
+    function sortByFirstName() {    
+            const sortedUsers = state.users.sort((current, next) => {
+                console.log(current, next)
+                if (current.name.first < next.name.first) {
+                    return -1
+                } else if (current.name.first > next.name.first) {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
+        
+        
         setState(
             {
                 ...state,
-                filterUsers: searchedUsers
+                filterUsers: sortedUsers
             }
         )
     }
+    function filterName(event) {
+        console.log(event.target.value)
+        const filteredUsers = state.users.filter((user) => {
+            const userFullName = user.name.first + " " + user.name.last
+            return userFullName.toLowerCase().includes(event.target.value.toLowerCase())
+        
+        
+        })
+        setState(
+        {
+            ...state,
+            filterUsers: filteredUsers
+        }
+    )}
 
     return (
         <div>
-            <p>EmployeeDirectory</p>
-            <div>
-                <button onClick={handleSearch}>"Click Me"</button>
-                {/* <SearchEmp></SearchEmp> */}
+            <p id="title">EmployeeDirectory</p>
+            <div class="filters">
+                <input onChange={filterName}></input>
+                <button onClick={sortByFirstName}>Sort by First Name</button>
+                <button onClick={sortByDOB}>Sort by Date of Birth</button>
             </div>
+
             {state.filterUsers.map(user => {
-                {console.log(user)}
+                // {console.log(user)}
                 return <Employee user={user}></Employee>
             })}
-            {console.log(state)}
+            {/* {console.log(state)} */}
         </div>
         
     )
